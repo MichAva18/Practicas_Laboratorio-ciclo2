5f5dd6ca5fe95555c8ce18f04b3125a6cd5744b9
Karla Michel <michaavalos18@gmail.com>
2026-08-26 13:26:10 -0600

    Practica 2

---
+++ practica2/ejemplo.js
@@ -0,0 +1,7 @@
+let nombre = 'Carlos';
+
+let nombreMin = nombre.toLowerCase()
+let nombreMayu = nombre.toLocaleUpperCase()
+
+ console.log("Su nombre es minusculas es: " +nombreMin);
+ console.log("Su nombre es mayusculas es: " +nombreMayu);
